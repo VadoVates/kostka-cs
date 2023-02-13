044a942bfb9d7868de2db03ef44898c6dabacbb7
VadoVates <121289099+VadoVates@users.noreply.github.com>
2023-02-13 20:42:31 +0100

    Działa

--- Program.cs
+++ Program.cs
@@ -11,24 +11,68 @@
             Console.WriteLine ("1 - kamień");
             Console.WriteLine ("2 - papier");
             Console.WriteLine ("3 - nożyce");
-            string[] tabela = {"kamień", "papier", "nożyce"};
-            string wyborGraczString;
-            int wyborGracz, wyborKomp, zwyciestwaKomp = 0, zwyciestwaGracz = 0;
-            wyborKomp=Convert.ToChar(losowa.Next(1,4));
-            for (int i=1;i<=10;i++)
+            Console.WriteLine ("0 - przerwanie gry");
+            string[] tabela = {"wyjście", "kamień", "papier", "nożyce"};
+            bool wyborGraczBool;
+            int i, wyborGracz, wyborKomp, zwyciestwaKomp = 0, zwyciestwaGracz = 0;
+            for (i=1;i<=10;i++)
             {
-                wyborKomp=losowa.Next(0,3);
+                wyborKomp=losowa.Next(1,4);
+                Console.WriteLine($"\nRUNDA {i}!");
                 Console.WriteLine ("Użytkowniku, podaj swój typ: ");
-                wyborGraczString = Console.ReadLine();
-                Console.WriteLine ($"Wybór komputera to: {tabela[wyborKomp]}");
-                Int32.TryParse (wyborGraczString, out wyborGracz);
-                if (wyborGracz!=1 && wyborGracz!=2 && wyborGracz!=3)
+                wyborGraczBool = (Int32.TryParse (Console.ReadLine(), out wyborGracz));
+                if (wyborGraczBool && wyborGracz!=0)
                 {
+                    Console.WriteLine ($"Wybór gracza to: {tabela[wyborGracz]}");
+                    Console.WriteLine ($"Wybór komputera to: {tabela[wyborKomp]}");
+                }
+                if (wyborGraczBool && (wyborGracz==1 || wyborGracz==2 || wyborGracz==3))
+                {
+                    if (wyborGracz==wyborKomp)
+                    {
+                        Console.WriteLine ("Mamy remis - powtórka rundy");
+                        i--;
+                    }
+                    else if ((wyborGracz==1 && wyborKomp==3) || (wyborGracz==2 && wyborKomp==1) || (wyborGracz==3 && wyborKomp==2))
+                    {
+                        Console.WriteLine ("Tę rundę wygrywa GRACZ!");
+                        zwyciestwaGracz++;
+                    }
+                    else
+                    {
+                        Console.WriteLine ("Tę rundę wygrywa KOMPUTER!");
+                        zwyciestwaKomp++;
+                    }
+                }
+                else
+                {
+                    if (wyborGraczBool && wyborGracz==0)
+                    {
+                        Console.WriteLine ("Wybrano przerwanie gry");
+                        break;
+                    }
                     Console.WriteLine ("Powtórz, wprowadzono nieprawidłową liczbę");
                     i--;
                 }
             }
-            
+            i--;
+            Console.WriteLine ($"\nOstateczny wynik po {i} rundach to:");
+            if (zwyciestwaGracz==zwyciestwaKomp)
+            {
+                Console.WriteLine ("REMIS OBU ZAWODNIKÓW");
+            }
+            if (zwyciestwaGracz>zwyciestwaKomp)
+            {
+                Console.WriteLine ("ZWYCIĘŻYŁ GRACZ!");
+            }
+            if (zwyciestwaGracz<zwyciestwaKomp)
+            {
+                Console.WriteLine ("ZWYCIĘŻYŁ KĄKUTER!");
+            }
+            Console.WriteLine ($"Wynik: CZŁOWIEK {zwyciestwaGracz} : {zwyciestwaKomp} KOMPUTER");
+            Console.WriteLine ("Wciśnij aby zakończyć");
+            Console.ReadKey(true);
+
         }
     }
 }
